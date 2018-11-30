8d77bab16b99d1bf94e11ae660fca8683a5c75e5
Ezra Augustin <elidoom88@gmail.com>
2018-11-30 16:30:34 +0100

    thermo 0°

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <html>
  <head>
+
+   <link href="index.css" type="text/css" rel="stylesheet">
   <title>Temprature Test</title>
+
  </head>
  <body>
 
    <h1>Température</h1>
 
-   {"temperature":"26","humidite":"45"}
-
-   <p>
- <img src="thermometer.jpg" alt="themo" id="photo" />
-   </p>
 
  <?php
 
+ $pageTemp = file_get_contents('data.txt');
+
+ $tuff = json_decode($pageTemp);
+
+
  ?>
+
+ <p>Il fait <?php echo $tuff->temperature; ?>° avec <?php echo $tuff->humidite ?>% d'humidité</p>
+
+
+ <div id="thermometer">
+   <div id="bargraph"> </div>
+ </div>
+
+
  </body>
 </html>
